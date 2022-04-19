9fa7c8f644006d8184ae3fd484fe79ef5e7a3d5a
EugeneMusic <muzychenko_eugene@mail.ru>
2022-04-19 23:04:35 +0300

    feat: add burger icon animation

---
+++ shelter/pages/main/script.js
@@ -0,0 +1,7 @@
+const burger = document.querySelector('.burger-icon');
+
+burger.addEventListener('click', () => {
+    burger.classList.toggle('burger-icon_active');
+    // menu.classList.toggle('menu_active');
+    // body.classList.toggle('body_active');
+});
